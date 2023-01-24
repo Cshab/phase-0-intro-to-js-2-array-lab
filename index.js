43a696e6c5b699634cfdadc52f699fcf4e60d113
Colby Shabel <jcshabel@gmail.com>
2023-01-23 22:11:56 -0600

    commit tree 0a54727cd7a6a86de22c1191bd200d633357121e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastCat(){
+    return cats.push("Ralph")
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.unshift("Bob")
+}
+
+cats.pop()
+ 
+cats.shift()
+
+const Newcats = cats.slice()
+
+Newcats.push("Broom")
+
+cats.slice(0,2)
+
+cats.slice(1)
